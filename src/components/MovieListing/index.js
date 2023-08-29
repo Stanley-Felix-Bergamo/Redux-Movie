97aaeb3b857d277f9db81ed5fd2c71d88c538029
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-08-29 16:58:31 -0300

    Commit

--- src/components/MovieListing/index.js
+++ src/components/MovieListing/index.js
@@ -1,7 +1,26 @@
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { getAllMovies } from '../../features/movies/movieSlice';
+import MovieCard from '../MovieCard/index';
+import './styles.scss';
+
 const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
   return (
-    <div>
-      <h1>MovieListing</h1>
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movies-container">
+          {movies.Search ? (
+            movies.Search.map((movie) => (
+              <MovieCard key={movie.imdbID} data={movie} />
+            ))
+          ) : (
+            <div className="movies-error">
+              <h3>{movies.Error}</h3>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,30 @@
+import MovieListing from '../../components/MovieListing/index';
+import movieApi from '../../commom/APIs/index';
+import { Key } from '../../commom/APIs/key';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const movieText = 'Back to the Future';
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${Key}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log('error:', err);
+        });
+      dispatch(addMovies(response.data));
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <div className="banner-img"></div>
+      <MovieListing />
     </div>
   );
 };
